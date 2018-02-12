1165f6361b9932d29b9ed58d5fc8b487de92a95a
Thiago Sanches <tsigwt@gmail.com>
2018-02-12 16:20:02 -0200

    update

---
+++ InterfaceBuilder.Engine/Elements/Checkbox/Window.cs
@@ -0,0 +1,32 @@
+﻿using InterfaceBuilder.Engine.Interface;
+using InterfaceBuilder.Model;
+using System.IO;
+using System.Reflection;
+
+namespace InterfaceBuilder.Engine.Elements
+{
+    internal class Checkbox : IElement
+    {
+        public string Build(object element)
+        {
+            InterfaceBuilder.Model.Window window = (InterfaceBuilder.Model.Window)element;
+            string template = GetTemplate();
+
+            return string.Format(template, window.Title);
+        }
+
+        private string GetTemplate()
+        {
+            string template = null;
+            var assembly = typeof(Builder).GetTypeInfo().Assembly;
+
+            using (StreamReader streamReader = new StreamReader(assembly.
+                GetManifestResourceStream("InterfaceBuilder.Engine.Elements.Checkbox.Template.html")))
+            {
+                template = streamReader.ReadToEnd();
+            }
+
+            return template;
+        }
+    }
+}
