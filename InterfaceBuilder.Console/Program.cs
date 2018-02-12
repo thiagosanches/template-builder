0665da684e3bb0f3e62291847fe162ba0392ceb8
Thiago Sanches <tsigwt@gmail.com>
2018-02-11 22:38:22 -0200

    update

--- InterfaceBuilder.Console/Program.cs
+++ InterfaceBuilder.Console/Program.cs
@@ -2,6 +2,7 @@
 using InterfaceBuilder.Model;
 using Newtonsoft.Json;
 using System.IO;
+using System.Reflection;
 
 namespace InterfaceBuilder.Console
 {
@@ -9,10 +10,22 @@ namespace InterfaceBuilder.Console
     {
         static void Main(string[] args)
         {
+            if(args != null &&
+                args.Length != 2)
+            {
+                System.Console.WriteLine("Hey, you have to pass two parameters: <my_input_json> <my_output_template>");
+                return;
+            }
+
             Builder builder = new Builder();
 
-            Window window = LoadFromFile(@"C:\Users\Thiago\repositories\interfacebuilder\example-hard.json");
-            InsertWindowIntoTemplate(builder.Build(window), @"C:\Users\Thiago\Downloads\Starter Template for Bootstrap.html");
+            Window window = LoadFromFile(args[0]);
+            string filePath = args[1];
+
+            string windowContent = builder.Build(window);
+            string template = GetTemplate();
+
+            InsertIntoTemplate(windowContent, template, filePath);
         }
 
         private static Window LoadFromFile(string filePath)
@@ -32,18 +45,31 @@ namespace InterfaceBuilder.Console
             return window;
         }
 
-        private static void InsertWindowIntoTemplate(string window, string filePath)
+        private static void InsertIntoTemplate(string window, string template, string filePath)
         {
             try
             {
-                string templateContent = File.ReadAllText(filePath);
-                File.WriteAllText(@"C:\Users\Thiago\Downloads\testing.html",
-                    templateContent.Replace("[WINDOW]", window, System.StringComparison.InvariantCulture));
+                File.WriteAllText(filePath, template.Replace("[WINDOW]",
+                    window, System.StringComparison.InvariantCulture));
             }
             catch (System.Exception ex)
             {
                 System.Console.WriteLine(ex.Message);
             }
+        }
+
+        private static string GetTemplate()
+        {
+            string template = null;
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+
+            using (StreamReader streamReader = new StreamReader(assembly.
+                GetManifestResourceStream("InterfaceBuilder.Console.Template.Template.html")))
+            {
+                template = streamReader.ReadToEnd();
+            }
+
+            return template;
         }
     }
 }
